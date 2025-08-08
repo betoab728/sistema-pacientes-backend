ddbe66093520ae01289a304e281cfeeae0b90fc2
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2025-08-08 11:22:02 -0500

    recupere archivo

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { getUsers, createUser,updateUser,loginUser, getUserById } from '../controllers/userController';
+
+const router = Router();
+
+router.post('/login', loginUser);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.get('/:id', getUserById);
+router.get('/', getUsers);
+
+export default router;
